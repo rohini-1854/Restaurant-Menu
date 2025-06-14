f23d677f879b6590d7d3a9e3915cf900035d8553
rohini-1854 <rohinirohymsu20@gmail.com>
2025-06-14 17:12:24 +0530

    Initial upload of Restaurant Menu website

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(menuType) {
+    document.getElementById(menuType).classList.add("show");
+}
+
+function closeModal(menuType) {
+    document.getElementById(menuType).classList.remove("show");
+}
+
